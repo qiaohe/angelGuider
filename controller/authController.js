6d93581a8ac698bab0587c6f431cec70e04932b6
qiao.he <qiao.he@gmail.com>
2016-10-26 18:10:58 +0800

    New changelist

--- controller/authController.js
+++ controller/authController.js
@@ -7,6 +7,7 @@ var i18n = require('../i18n/localeMessage');
 var _ = require('lodash');
 var moment = require('moment');
 var uuid = require('node-uuid');
+var ic = require("../common/invitationCode");
 module.exports = {
     login: function (req, res, next) {
         var userName = (req.body && req.body.username) || (req.query && req.query.username);
@@ -77,5 +78,80 @@ module.exports = {
         }).catch(function (err) {
             res.send({ret: 1, message: err.message});
         });
+    },
+    register: function (req, res, next) {
+        var user = req.body;
+        var invitationCode = user.invitationCode;
+        redis.getAsync(user.mobile).then(function (reply) {
+            if (!(reply && reply == user.certCode))  throw new Error('无效的验证码。');
+            return angelGuiderDAO.findByMobile(user.mobile).then(function (users) {
+                if (users.length) throw new Error('手机号码已存在');
+                user = _.assign(_.omit(user, ['certCode']), {
+                    createDate: new Date(),
+                    invitationCode: ic.generate(),
+                    gender: 0,
+                    status: 0,
+                    balance: 0.00,
+                    password: md5(req.body.password)
+                });
+                if (invitationCode) {
+                    return angelGuiderDAO.findByInvitationCode(invitationCode).then(function (invitee) {
+                        if (invitee && invitee.length < 1) throw new Error('无效的邀请码');
+                        user.agency = invitee[0].id;
+                        user.agencyName = invitee[0].realName;
+                        return angelGuiderDAO.insert(user);
+                    })
+                } else {
+                    return angelGuiderDAO.insert(user);
+                }
+            }).then(function (result) {
+                var uid = result.insertId;
+                user.id = uid;
+                return angelGuiderDAO.insertAccount({
+                    uid: uid,
+                    updateDate: new Date(),
+                    accountNo: 'B' + user.agency + '-' + moment().format('YYMMDD') + uid,
+                    balance: 0.00,
+                    // accountName: req.body.realName,
+                    availableBalance: 0.00,
+                    status: 0,
+                    type: 0
+                });
+            }).then(function (result) {
+                var token = uuid.v4();
+                redis.set(token, JSON.stringify(user));
+                redis.expire(token, config.app.tokenExpire);
+                user.token = token;
+                delete user.password;
+                user.gender = config.gender[user.gender];
+                redis.getAsync('b:uid:' + user.id + ':token').then(function (reply) {
+                    redis.del(reply);
+                    redis.set('b:uid:' + user.id + ':token', token);
+                });
+                res.send({ret: 0, data: user});
+            })
+        }).catch(function (err) {
+            res.send({ret: 1, message: err.message});
+        });
+        return next();
+    },
+
+    getInvitation: function (req, res, next) {
+        var uid = req.user.id;
+        var data = _.cloneDeep(config.invitationTemplate);
+        angelGuiderDAO.findById(req.user.id).then(function (users) {
+            data.url = data.url + users[0].invitationCode;
+            data.shareUrl = data.shareUrl + users[0].invitationCode;
+            if (users[0].invitationCode) return res.send({ret: 0, data: data});
+            var code = ic.generate();
+            return angelGuiderDAO.update({id: users[0].id, invitationCode: code}).then(function (result) {
+                data.url = data.url + code;
+                data.shareUrl = data.shareUrl + users[0].invitationCode;
+                res.send({ret: 0, data: data});
+            });
+        }).catch(function (err) {
+            res.send({ret: 1, message: err.message});
+        });
+        return next();
     }
 }
